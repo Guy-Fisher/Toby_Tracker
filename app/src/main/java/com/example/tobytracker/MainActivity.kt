3ce2eb8905e0d7a9d3d63a531bc2c35070ca03ed
Guy-Fisher <guystephenfisher@gmail.com>
2023-11-12 00:53:56 +0000

    Added separate trackers for each participant Added exception handling for negative numbers and none Int values

--- app/src/main/java/com/example/tobytracker/MainActivity.kt
+++ app/src/main/java/com/example/tobytracker/MainActivity.kt
@@ -9,45 +9,59 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private var myKilometers = 0
-    private lateinit var myKilometersTextView: TextView
     private var hisKilometers = 0
+    private lateinit var myKilometersTextView: TextView
     private lateinit var hisKilometersTextView: TextView
+    private lateinit var myKilometersInput: EditText
+    private lateinit var hisKilometersInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         myKilometersTextView = findViewById(R.id.myKilometersTextView)
-
         hisKilometersTextView = findViewById(R.id.hisKilometersTextView)
+        myKilometersInput = findViewById<EditText>(R.id.kilometersInput)
+        hisKilometersInput = findViewById<EditText>(R.id.hisKilometersInput)
 
         val addKilometersButton = findViewById<Button>(R.id.addKilometersButton)
-        val kilometersInput = findViewById<EditText>(R.id.kilometersInput)
         val hisAddKilometersButton = findViewById<Button>(R.id.hisAddKilometersButton)
-        val hisKilometersInput = findViewById<EditText>(R.id.hisKilometersInput)
 
         addKilometersButton.setOnClickListener {
-            // Get the input from the EditText
-            val kilometersToAdd = kilometersInput.text.toString().toInt()
-
-            // Update your kilometers and refresh the UI
-            myKilometers += kilometersToAdd
-            myKilometersTextView.text = "Your Kilometers: $myKilometers"
-
+            myKilometers = handleAddKilometers(myKilometers, myKilometersTextView, myKilometersInput, "My Kilometers: ")
             // Clear the input field
-            kilometersInput.text.clear()
+            myKilometersInput.text.clear()
         }
 
         hisAddKilometersButton.setOnClickListener {
-            // Get the input from the EditText
-            val kilometersToAdd = hisKilometersInput.text.toString().toInt()
-
-            // Update your kilometers and refresh the UI
-            hisKilometers += kilometersToAdd
-            hisKilometersTextView.text = "Toby's Kilometers: $hisKilometers"
+            hisKilometers = handleAddKilometers(hisKilometers, hisKilometersTextView, hisKilometersInput, "Toby's Kilometers: ")
 
             // Clear the input field
             hisKilometersInput.text.clear()
         }
+    }
+    private fun handleAddKilometers(oldKilometers: Int, textView: TextView, newKilometers: EditText, preText: String): Int {
+
+        val kilometresToAdd = try {
+            val inputNumber = newKilometers.text.toString().toInt()
+
+            if (inputNumber < 0) {
+                return oldKilometers
+            }
+            else{
+                inputNumber
+            }
+        } catch (e: NumberFormatException) {
+            // Handle the case where a non-integer value is entered
+            // Display a message or handle it as appropriate
+            return oldKilometers
+        }
+
+        // Update kilometers and refresh the UI
+        val updatedKilometers = oldKilometers + kilometresToAdd
+
+        textView.text = "$preText$updatedKilometers"
+
+        return updatedKilometers
     }
 }
